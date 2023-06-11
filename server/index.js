7731bfe89f5a30ce95ae2c21417c6aa8dc478288
Hua Trung Kien <huatrungkien126@gmail.com>
2023-06-11 23:05:34 +0700

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,20 @@
+//CLI: npm install express body-parser --save
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+// middleware
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+// simple route API
+app.get('/hello', (req, res) => {
+    res.json({ message: 'Welcome to HELL.' });
+});
+
+// apis
+app.use('/api/admin', require('./api/admin.js'));
+app.use('/api/customer', require('./api/customer.js'));
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shoppeclone.w14pp4b.mongodb.net',
+    DB_USER: 'huatrungkien126',
+    DB_PASS: 'kien19032002',
+    DB_DATABASE: 'shoppeClone',
+    EMAIL_USER: '<email_user>', // Microsoft mail service
+    EMAIL_PASS: '<email_pass>',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
