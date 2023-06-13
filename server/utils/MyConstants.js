655b62edb7cbb05150b453063ddab333363d17be
Kien <huatrungkien126@gmail.com>
2023-06-13 16:35:23 +0700

    update data from utils

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -4,7 +4,7 @@ const MyConstants = {
     DB_PASS: 'kien19032002',
     DB_DATABASE: 'shoppeClone',
     EMAIL_USER: 'huatrungkien126@gmail.com', // Microsoft mail service
-    EMAIL_PASS: '0938604762Kien',
+    EMAIL_PASS: 'lpoknwqknmmrdwgs',
     JWT_SECRET: '<jwt_secret>',
     JWT_EXPIRES: '3600000', // in milliseconds
   };
