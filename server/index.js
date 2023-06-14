904670ea66565473267d7d470bc713ac4623b921
Hua Trung Kien <huatrungkien126@gmail.com>
2023-06-15 00:34:43 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,'192.168.101.2', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 // middleware
